11e1945ce22b8cf35eec519fe1dbde75ee5a34e1
msalem-tech <mohamedsalemmohamedalii90@gmail.com>
2026-05-04 23:11:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-// رسالة عند فتح الموقع
-alert("🚨 الموقع لسه تحت التطوير، استنى التحديثات 🔥");
 
-// إضافة فيديو
-function addVideo() {
-    let link = prompt("حط لينك الفيديو:");
-
-    if(link){
-        let container = document.getElementById("videos");
-
-        let a = document.createElement("a");
-        a.href = link;
-        a.target = "_blank";
-        a.innerText = "🎬 فيديو جديد";
-
-        container.appendChild(a);
-    }
-}
