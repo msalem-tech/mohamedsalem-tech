76c206d514c84fb24554f6f6a8746dfd667bd00a
msalem-tech <mohamedsalemmohamedalii90@gmail.com>
2026-05-07 02:24:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     const featuredBtn = document.getElementById('featured-btn');
-    const recentVideosBtn = document.getElementById('recent-videos-btn');
 
     if (featuredBtn) {
         featuredBtn.addEventListener('click', (e) => {
             e.preventDefault();
             alert("ما قولنا الموقع تحت التطوير 🙄😂");
-        });
-    }
-
-    if (recentVideosBtn) {
-        recentVideosBtn.addEventListener('click', (e) => {
-            e.preventDefault();
-            alert("استنى شوية.. بنرتب الفيديوهات وهتشتغل علطول 🎬⏳");
         });
     }
 });
